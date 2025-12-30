22814c5eb3f08dcaa82b88b6848b5d2305efb5cc
Elil Thirumugam <elilgrade8@gmail.com>
2025-12-30 16:58:49 -0500

    Added to GUI

--- ChessLibrary.py
+++ ChessLibrary.py
@@ -1,3 +1,37 @@
 import chess
+from nicegui import ui
 bot = chess.Board()
-bot.push_san(list(bot.legal_moves)[0].uci())
+file_path = "output.txt"
+
+# File write happens after every move:
+# with open(file_path, 'w') as file:
+#     file.write(str(bot))
+hashmap = {
+    "r":"♜",
+    "n":"♞",
+    "b":"♝",
+    "q":"♛",
+    "k":"♚",
+    "p":"♟",
+    "R":"♖",
+    "N":"♘",
+    "B":"♗",
+    "Q":"♕",
+    "K":"♔",
+    "P":"♙",
+    ".":""
+}
+status = []
+with open(file_path,'r') as file:
+    for line in file:
+        print(line.strip())
+        status.append(line.strip())
+print(status)
+for row in status:
+    row = row.split(" ")
+    with ui.row():
+        for col in row:
+            print(hashmap[col])
+            ui.label(hashmap[col])
+    print(row)
+ui.run()
