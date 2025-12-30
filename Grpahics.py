9996a056bea5fa672e3ceb9683ca546cabf0bf87
Elil Thirumugam <elilgrade8@gmail.com>
2025-12-30 12:31:46 -0500

    matrix

--- Grpahics.py
+++ Grpahics.py
@@ -1,7 +1,13 @@
 from nicegui import ui
 import ChessLibrary as robot
 
-ui.label('Hello World').classes("text-blue")
-print(robot)
+
+for i in range(1,9):
+    num = ""
+    for j in range(1,10):
+        num += str(i) + str(j)
+        num += " "
+    ui.label(num).classes("text-blue")
+print(robot.bot)
 
 ui.run()
