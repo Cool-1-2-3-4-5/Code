e581b84a5eb6d9122c8ca604700730ac2e3957cf
Elil Thirumugam <elilgrade8@gmail.com>
2026-01-15 21:22:56 -0500

    Day8

--- ML.py
+++ ML.py
@@ -17,17 +17,37 @@ import numpy as np
 # cap.release()
 # cv2.destroyAllWindows()
 
+
+#Parametres
+def empty():
+    pass
+cv2.namedWindow("Parametres")
+cv2.createTrackbar("H_Min","Parametres",0,255,empty)
+cv2.createTrackbar("S_Min","Parametres",0,255,empty)
+cv2.createTrackbar("V_Min","Parametres",0,255,empty)
+cv2.createTrackbar("H_Max","Parametres",0,255,empty)
+cv2.createTrackbar("S_Max","Parametres",0,255,empty)
+cv2.createTrackbar("V_Max","Parametres",0,255,empty)
+# Find position of the chess
+def position(piece_x,piece_y):
+    x_pos = (piece_x - board_x)/board_width
+    x_pos = round(x_pos)
+    y_pos = (piece_y - board_y)/board_height
+    y_pos = round(y_pos)
+    return (x_pos+1),(y_pos+1)
+
 # Chessboard detection
-cap = cv2.VideoCapture(0) # intialize
-x1,y1,x2,y2 = 0,0,0,0
+print("HI")
+cap = cv2.VideoCapture(1) # intialize
+board_x,board_y,board_width,board_height = 0,0,0,0
 while True:
     check, frame = cap.read()
     if not check:
         print("NOT Wokring")
         break
     hue = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
-    colour_lower = np.array([0,0,0])
-    colour_higher = np.array([255,255,58])
+    colour_lower = np.array([cv2.getTrackbarPos("H_Min","Parametres"),cv2.getTrackbarPos("S_Min","Parametres"),cv2.getTrackbarPos("V_Min","Parametres")])
+    colour_higher = np.array([cv2.getTrackbarPos("H_Max","Parametres"),cv2.getTrackbarPos("S_Max","Parametres"),cv2.getTrackbarPos("V_Max","Parametres")])
     object = cv2.inRange(hue, colour_lower, colour_higher)    
     countours, hierachry = cv2.findContours(object, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     x, y, width, height = 0,0,0,0
@@ -35,18 +55,24 @@ while True:
         noise = cv2.contourArea(i)
         if noise > 3000:
             x, y, width, height = cv2.boundingRect(i)
-            print(str(x))
+            # print(str(x))
             cv2.rectangle(frame, (x, y), (x+width, y+height), (0, 255, 0), 2)
     cv2.imshow("Detection",frame)
     cv2.imshow("d",object)
     if cv2.waitKey(2) & 0xFF == ord('d'): # id d is pressed and
-        x1,y1 = x, y
-        x2,y2 = width, height
+        board_x,board_y = x, y
+        board_width,board_height = width, height
         capture = frame.copy() 
         break
-print("X1: " + str(x1) + " Y1: " + str(y1) + " X2: " + str(x2) + " Y2: " + str(y2))
+print("X1: " + str(board_x) + " Y1: " + str(board_y) + " X2: " + str(board_x+board_width) + " Y2: " + str(board_y+board_height))
+board_width = board_width/8
+board_height = board_height/8
+
+#Draw Board
 for i in range(8):
-    cv2.rectangle(capture,(int(x1*(x2*i)),int(y1)),(int(x1*(x2+1+i)),int(y2)),(255,0,0),1)
+    for j in range(8):
+        cv2.rectangle(capture,(int(board_x+(board_width*j)),int(board_y+(board_height*i))),(int(board_x+(board_width*(j+1))),int(board_y+(board_height*(i+1)))),(255,0,0),1)
+
 cv2.imshow("NewFrame", capture)
 while True:
     if cv2.waitKey(2) & 0xFF == ord('d'):
